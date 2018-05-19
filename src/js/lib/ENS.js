d616f76f3bc88a34a550bd1c353c546308efec3c
pw94 <pawelmwinnicki@gmail.com>
2018-05-19 16:41:35 +0200

    ens mockup

--- src/js/lib/ENS.js
+++ src/js/lib/ENS.js
@@ -1,10 +1,33 @@
+const USE_MOCKUP = true;
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
 export default class ENS {
+    static address;
     getIdentityAddressByUsername(username) {
         return new Promise((resolve) => {
-            /**
-             * TODO: napisac komunikacje z ENS i mockup
-             */
-            resolve('0x00');
+            if (USE_MOCKUP) {
+                if (typeof address !== 'undefined') {
+                    resolve(address);
+                } else {
+                    resolve(ZERO_ADDRESS);
+                }
+            } else {
+                /**
+                 * TODO: Integrate with ENS
+                 */
+            }
+        })
+    }
+
+    register(username, newAddress) {
+        return new Promise((resolve) => {
+            if (USE_MOCKUP) {
+                address = newAddress;
+            } else {
+                /**
+                 * TODO: Integrate with ENS
+                 */
+            }
         })
     }
 }
